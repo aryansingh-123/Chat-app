97d4f82f7af9df1a055f0436d246d2bb6c56ab79
aryansingh-123 <aksingh2558@gmail.com>
2024-03-23 20:10:59 +0530

    fuctionality of sign up signin

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ import Form from './modules/Form';
 import { Routes , Route } from 'react-router-dom';
 function App() {
   return (
-    <div className =" bg-[#edf3fc] h-screen flex justify-center item-center">
-     {/*<Form/> */}
-     <Dashboard/>
-    </div>
+    <Routes>
+      <Route path='/' element={<Dashboard />} />
+      <Route path='/users/sign_in' element={<Form isSignInPage={true}/>} />
+      <Route path='/users/sign_up' element={<Form isSignInPage={false}/>} />
+    </Routes>
   );
 }
 
--- src/modules/Form/index.js
+++ src/modules/Form/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import Input from '../../components/input'
 import Button from '../../components/Button'
+import { useNavigate } from 'react-router-dom'
 const Form = (
     {
         isSignInPage = true,
@@ -15,17 +16,20 @@ const Form = (
         password:''
 
     })
+    const navigate = useNavigate()
     return (
-        <div className="bg-gray-200 w-[400px] h-[450px] shadow-lg hover:shadow-gray-900 shadow-gray-500 rounded-lg flex flex-col justify-center item-center">
-            <div className="text-4xl text-gray-700 font-extrabold flex justify-center item-center">Welcome {isSignInPage && 'Back'}</div>
-            <div className="text-xl font-light flex justify-center item-center">{isSignInPage ? 'Sign in to get explored' :'Sign Up to get started' }</div>
-            <form onSubmit={()=> console.log('Submitted')}>
-           { !isSignInPage &&<div className="px-5 py-1"> <Input label="Full name" name="name" placeholder="Enter your Full Name" value={data.fullName} onChange={(e) => setData({...data, fullName: e.target.value})}/></div>}
-           <div className="px-5 py-1"> <Input label="Email" name="Email" placeholder="Enter your Email" value={data.email} onChange={(e) => setData({...data, email: e.target.value})}/></div>
-           <div className="px-5 py-1"> <Input label="Password" name="Password" placeholder="Enter your Password" value={data.password} onChange={(e) => setData({...data, password: e.target.value})}/></div>
-           <Button type='submit' label={isSignInPage ? 'Sign In': 'Sign Up'}/>
-           </form>
-           <div className="text-sm flex justify-center item-center">{ isSignInPage ? "Didn't have an account?" : "Already have an Account?"} <span className="text-blue-700 cursor-pointer underline" >{isSignInPage ? 'Sign Up' :'Sign In'} </span></div>
+        <div className='bg-gray-250 h-screen py-10 flex  item-center justify-center '>
+            <div className="bg-gray-200 w-[500px] h-[550px] shadow-lg hover:shadow-gray-900 shadow-gray-500 rounded-lg flex flex-col justify-center item-center">
+                <div className="text-4xl text-gray-700 font-extrabold flex justify-center item-center">Welcome {isSignInPage && 'Back'}</div>
+                <div className="text-xl font-light flex justify-center item-center">{isSignInPage ? 'Sign in to get explored' :'Sign Up to get started' }</div>
+                <form onSubmit={()=> console.log('Submitted')}>
+               { !isSignInPage &&<div className="px-5 py-1"> <Input label="Full name" name="name" placeholder="Enter your Full Name" value={data.fullName} onChange={(e) => setData({...data, fullName: e.target.value})}/></div>}
+               <div className="px-5 py-1"> <Input label="Email" name="Email" placeholder="Enter your Email" value={data.email} onChange={(e) => setData({...data, email: e.target.value})}/></div>
+               <div className="px-5 py-1"> <Input label="Password" name="Password" placeholder="Enter your Password" value={data.password} onChange={(e) => setData({...data, password: e.target.value})}/></div>
+               <Button type='submit' label={isSignInPage ? 'Sign In': 'Sign Up'}/>
+               </form>
+               <div className="text-sm flex justify-center item-center" onClick={()=> navigate(`/users/${isSignInPage ? 'sign_up' : 'sign_in'}`)}>  { isSignInPage ? "Didn't have an account?" : "Already have an Account?"} <span className="text-blue-700 cursor-pointer underline" >{isSignInPage ? 'Sign Up' :'Sign In'} </span></div>
+            </div>
         </div>
     )
 }
